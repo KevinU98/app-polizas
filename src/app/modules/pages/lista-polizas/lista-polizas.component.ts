dd9f4de62ebd7e996834af48f4dda9a5454cb33a
KevinUriegas98 <102544692+KevinUriegas98@users.noreply.github.com>
2024-12-19 22:44:05 -0600

    Update lista-polizas.component.ts

--- src/app/modules/pages/lista-polizas/lista-polizas.component.ts
+++ src/app/modules/pages/lista-polizas/lista-polizas.component.ts
@@ -55,9 +55,9 @@ export class ListaPolizasComponent implements OnInit{
           (data) => {
             const beneficiarios = data.response.data.map((benef, index) => `
               <tr>
-                <td class="border border-slate-700 p-2" colspan="2"><strong>BENEFICIARIO (${index + 1}): </strong>${benef.Nombre}</td>
-                <td class="border border-slate-700 p-2"><strong>EDAD: </strong>${benef.Edad}</td>
-                <td class="border border-slate-700 p-2"><strong>PARENTESCO: </strong>${benef.Parantesco}</td>
+                <td class="border border-slate-500 p-2" colspan="2"><strong>BENEFICIARIO (${index + 1}): </strong>${benef.Nombre}</td>
+                <td class="border border-slate-500 p-2"><strong>EDAD: </strong>${benef.Edad}</td>
+                <td class="border border-slate-500 p-2"><strong>PARENTESCO: </strong>${benef.Parantesco}</td>
               </tr>
             `).join('');
             resolve(beneficiarios);
@@ -72,58 +72,126 @@ export class ListaPolizasComponent implements OnInit{
       return;
     }
     const content = `
+    <style>
+      table {
+        page-break-inside: avoid;
+      }
+      tr {
+        page-break-inside: avoid;
+      }
+      td {
+        page-break-inside: avoid;
+      }
+    </style>
     <div class="grid grid-cols-10 items-center ">
-    <div class="col-span-7 flex flex-col items-left">
-      <img src="../../../assets/dist/image/logo.png" class="h-[130px] w-[300px]" alt="Logotipo" />
-      <p class="text-sm font-bold">Calle Miguel Hidalgo #505 Nte, Col. Centro, Linares, N.L. Tel: 821-110-47-45.</p>
+      <div class="col-span-7 flex flex-col items-left">
+        <img src="../../../assets/dist/image/logo.png" class="h-[130px] w-[300px]" alt="Logotipo" />
+        <p class="text-sm font-bold">Calle Miguel Hidalgo #505 Nte, Col. Centro, Linares, N.L. Tel: 821-110-47-45.</p>
+      </div>
+      <div class="col-span-3 bg-[#21B2F9] text-center text-sm">
+          <h4 class="text-white p-4">Póliza de Seguro</h4>
+      </div>
     </div>
-    <div class="col-span-3 text-center text-sm">
-        <table class="border-collapse border border-slate-500 w-full">
-            <tbody>
-                <tr>
-                    <td class="border border-slate-700 p-2 bg-[#21B2F9] text-white"><strong>FOLIO: </strong></td>
-                    <td class="border border-slate-700 p-2 bg-[#21B2F9] text-white"><strong>FECHA: </strong></td>
-                </tr>
-                <tr>
-                    <td class="border border-slate-700 p-2 font-bold">N° ${poliza.Id}</td>
-                    <td class="border border-slate-700 p-2 font-bold">${poliza.Fecha}</td>
-                </tr>
-            </tbody>
-        </table>
-        <!-- <h4 class="text-white p-4">Póliza de Seguro</h4> -->
-    </div>
-  </div>
-
-    <div class="mt-3 text-xs">
-      <table class="border-collapse border border-slate-500 w-full">
+    <div class="mt-3 border-collapse border border-slate-500 text-xs">
+      <table class="w-full">
+        <tbody>
+          <tr>
+            <td class="p-2 border border-slate-500"><strong>PÓLIZA: </strong>35063-02</td>
+            <td class="p-2 border border-slate-500"><strong>CONSECUTIVO: </strong>49341</td>
+            <td class="p-2 border border-slate-500"><strong>AGENTE: </strong>99</td>
+            <td class="p-2 border border-slate-500"><strong>OFICINA: </strong>MATRIZ</td>
+          </tr>
+          <tr>
+            <td class="p-2 border border-slate-500"><strong>AGRUPADOR: </strong></td>
+            <td class="p-2 border border-slate-500"><strong>MONEDA: </strong>M.N.</td>
+            <td class="p-2 border border-slate-500"><strong>FORMA PAGO: </strong>ANUAL</td>
+            <td class="p-2 border border-slate-500"><strong>DIAS VIGENCIA: </strong>365</td>
+          </tr>
+          <tr>
+            <td class="p-2 border border-slate-500"><strong>ID CLIENTE: </strong>0001508628</td>
+            <td class="p-2 border border-slate-500" colspan="2"><strong>FORMA ADMINISTRACIÓN: </strong>N/A</td>
+            <td class="p-2 border border-slate-500"><strong>MEDIO PAGO: </strong>ANUAL</td>
+          </tr>
+          <tr>
+            <td class="text-center p-21 border border-slate-500"><strong>DESDE HRS.</strong></td>
+            <td class="text-center p-2 border border-slate-500"><strong>HASTA HRS.</strong></td>
+            <td class="text-center p-2 border border-slate-500"><strong>FECHA DE EMISIÓN</strong></td>
+            <td class="text-center p-2 border border-slate-500"><strong>PLAN</strong></td>
+          </tr>
+          <tr>
+            <td class="text-center p-2 border border-slate-500">31/12/2024 12:00</td>
+            <td class="text-center p-2 border border-slate-500">31/12/2025 12:00</td>
+            <td class="text-center p-2 border border-slate-500">31/12/2024</td>
+            <td class="text-center p-2 border border-slate-500">PLAN MAESTRO ACCIDENTES</td>
+          </tr>
+        </tbody>
+      </table>
+      </div>
+      <table class="border-collapse border border-slate-500 w-full text-xs">
           <tbody>
             <tr>
-                <td class="border border-slate-700 p-2 bg-[#21B2F9] text-white text-center" colspan="4"><strong>DATOS DEL CONTRATANTE</strong></td>
+                <td class="border border-slate-500 p-2 bg-[#21B2F9] text-white text-center" colspan="4"><strong>DATOS DEL CONTRATANTE</strong></td>
             </tr>
             <tr>
-                <td class="border border-slate-700 p-2" colspan="4"><strong>NOMBRE: </strong>${poliza.Nombre}</td>
+                <td class="border border-slate-500 p-2" colspan="4"><strong>NOMBRE: </strong>${poliza.Nombre}</td>
             </tr>
             <tr>
-                <td class="border border-slate-700 p-2" colspan="3"><strong>DIRECCIÓN PARTICULAR: </strong>${poliza.DireccionParticular}</td>
-                <td class="border border-slate-700 p-2"><strong>POBLACIÓN: </strong>${poliza.Poblacion}</td>
+                <td class="border border-slate-500 p-2" colspan="3"><strong>DIRECCIÓN PARTICULAR: </strong>${poliza.DireccionParticular}</td>
+                <td class="border border-slate-500 p-2"><strong>POBLACIÓN: </strong>${poliza.Poblacion}</td>
             </tr>
             <tr>
-                <td class="border border-slate-700 p-2" colspan="4"><strong>DIRECCIÓN DE COBRO: </strong>${poliza.DomicilioCobro}</td>
+                <td class="border border-slate-500 p-2" colspan="4"><strong>DIRECCIÓN DE COBRO: </strong>${poliza.DomicilioCobro}</td>
             </tr>
             <tr>
-                <td class="border border-slate-700 p-2 bg-[#21B2F9] text-white text-center" colspan="4"><strong>DATOS DE LA EMPRESA EN QUE TRABAJA</strong></td>
+                <td class="border border-slate-500 p-2 bg-[#21B2F9] text-white text-center" colspan="4"><strong>DATOS DE LA EMPRESA EN QUE TRABAJA</strong></td>
             </tr>
             <tr>
-                <td class="border border-slate-700 p-2" colspan="4"><strong>NOMBRE: </strong>${poliza.Empresa}</td>
+                <td class="border border-slate-500 p-2" colspan="4"><strong>NOMBRE: </strong>${poliza.Empresa}</td>
             </tr>
             <tr>
-                <td class="border border-slate-700 p-2" colspan="3"><strong>CALLE: </strong>${poliza.CalleEmpresa}</td>
-                <td class="border border-slate-700 p-2"><strong>TEL: </strong>${poliza.TelEmpresa}</td>
+                <td class="border border-slate-500 p-2" colspan="3"><strong>CALLE: </strong>${poliza.CalleEmpresa}</td>
+                <td class="border border-slate-500 p-2"><strong>TEL: </strong>${poliza.TelEmpresa}</td>
             </tr>
             <tr>
-                <td class="border border-slate-700 p-2 bg-[#21B2F9] text-white text-center" colspan="4"><strong>BENEFICIARIOS</strong></td>
+                <td class="border border-slate-500 p-2 bg-[#21B2F9] text-white text-center" colspan="4"><strong>BENEFICIARIOS</strong></td>
             </tr>
             ${beneficiariosHTML}
+        </tbody>
+      </table>
+    </div>
+    <div class="text-xs">
+      <table class="border-collapse border border-slate-500 w-full">
+        <tbody>
+          <tr>
+            <td class="border border-slate-500 p-2 bg-[#21B2F9] text-white text-center" colspan="2"><strong>BENEFICIOS CUBIERTOS</strong></td>
+            <td class="border border-slate-500 p-2 bg-[#21B2F9] text-white text-center"><strong>SUMA ASEGURADA </strong></td>
+            <td class="border border-slate-500 p-2 bg-[#21B2F9] text-white text-center"><strong>DEDUCIBLE </strong></td>
+          </tr>
+          <tr>
+            <td class="border border-slate-500 p-2" colspan="2">GASTOS FUNERARIOS POR ACCIDENTE</td>
+            <td class="border border-slate-500 p-2">$ 50,000.00</td>
+            <td class="border border-slate-500 p-2"></td>
+          </tr>
+          <tr>
+            <td class="border border-slate-500 p-2" colspan="2">MUERTE ACCIDENTAL</td>
+            <td class="border border-slate-500 p-2">$ 300,000.00</td>
+            <td class="border border-slate-500 p-2"></td>
+          </tr>
+          <tr>
+            <td class="border border-slate-500 p-2" colspan="2">PERDIDAS ORGÁNICAS ESCALA B</td>
+            <td class="border border-slate-500 p-2">$ 200,000.00</td>
+            <td class="border border-slate-500 p-2"></td>
+          </tr>
+          <tr>
+            <td class="border border-slate-500 p-2" colspan="2">GASTOS MÉDICOS POR ACCIDENTE</td>
+            <td class="border border-slate-500 p-2">$ 200,000.00</td>
+            <td class="border border-slate-500 p-2"></td>
+          </tr>
+          <tr>
+            <td class="border border-slate-500 p-2" colspan="2">INDEMNIZACIÓN DIARIA POR HOSPITALIZACIÓN</td>
+            <td class="border border-slate-500 p-2">$ 833.33</td>
+            <td class="border border-slate-500 p-2"></td>
+          </tr>
         </tbody>
       </table>
     </div>
@@ -131,12 +199,12 @@ export class ListaPolizasComponent implements OnInit{
       <table class="border-collapse border border-slate-500 w-full">
         <tbody>
           <tr>
-            <td class="border border-slate-700 p-2 bg-[#21B2F9] text-white text-center"><strong>NOMBRE DEL VENDEDOR: </strong></td>
-            <td class="border border-slate-700 p-2 bg-[#21B2F9] text-white text-center"><strong>NOMBRE DEL PROMOTOR: </strong></td>
+            <td class="border border-slate-500 p-2 bg-[#21B2F9] text-white text-center"><strong>NOMBRE DEL VENDEDOR: </strong></td>
+            <td class="border border-slate-500 p-2 bg-[#21B2F9] text-white text-center"><strong>NOMBRE DEL PROMOTOR: </strong></td>
           </tr>
           <tr>
-            <td class="border border-slate-700 p-2 text-center">${poliza.Vendedor}</td>
-            <td class="border border-slate-700 p-2 text-center">${poliza.Promotor}</td>
+            <td class="border border-slate-500 p-2 text-center">${poliza.Vendedor}</td>
+            <td class="border border-slate-500 p-2 text-center">${poliza.Promotor}</td>
           </tr>
         </tbody>
       </table>
@@ -155,7 +223,7 @@ export class ListaPolizasComponent implements OnInit{
       <table class="w-full">
         <tbody>
           <tr class="bg-[#21B2F9] text-white">
-            <td class="font-bold border border-slate-700 p-2 text-center text-sm" colspan="2">El servicio funerario incluye</td>
+            <td class="font-bold border border-slate-500 p-2 text-center text-sm" colspan="2">El servicio funerario incluye</td>
           </tr>
           <tr>
             <td class="p-2 text-sm w-1/2"><strong>1.</strong> Gestión del funeral.</td>
